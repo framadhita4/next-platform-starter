a0979173948bb6f6b383e89e267d54fc9b7e69ae
framadhita4 <framadhita@outlook.com>
2024-12-30 16:07:32 +0700

    -

---
+++ app/test/[slug]/page.jsx
@@ -0,0 +1,15 @@
+import { use } from "react"
+
+const Page = ({ params: paramsPromise }) => {
+  const params = use(paramsPromise);
+
+  const {slug} = params;
+
+  return (
+    <div>
+      <h1>Page {slug}</h1>
+    </div>
+  )
+}
+
+export default Page
