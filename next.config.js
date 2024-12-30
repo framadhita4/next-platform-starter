497a6a847777b4aa287d5277c0a12d0d2bf121b8
framadhita4 <framadhita@outlook.com>
2024-12-30 16:45:00 +0700

    -

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    experimental: {
+        turbo: {
+          resolveExtensions: [
+            '.mdx',
+            '.tsx',
+            '.ts',
+            '.jsx',
+            '.js',
+            '.mjs',
+            '.json',
+          ],
+        }
+      }
 };
 
 module.exports = nextConfig;
